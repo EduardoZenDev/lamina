3790c132dd144818cb5d444eb9513d0f16720bfa
EduardoZenDev <lalozen68@gmail.com>
2025-01-27 13:45:03 -0600

    Datos de laminas 9

--- config/container.php
+++ config/container.php
@@ -3,13 +3,31 @@
 use Laminas\Mvc\Application;
 use Laminas\Stdlib\ArrayUtils;
 
-// Retrieve configuration
-$appConfig = require __DIR__ . '/application.config.php';
+// Activa el modo de depuración temporalmente
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Verifica si el archivo application.config.php existe
+$configFile = __DIR__ . '/application.config.php';
+if (!file_exists($configFile)) {
+    die('Error: application.config.php no encontrado.');
+}
+
+// Carga la configuración
+$appConfig = require $configFile;
 if (file_exists(__DIR__ . '/development.config.php')) {
-    /** @var array $devConfig */
     $devConfig = require __DIR__ . '/development.config.php';
     $appConfig = ArrayUtils::merge($appConfig, $devConfig);
 }
 
-return Application::init($appConfig)
-    ->getServiceManager();
+// Verifica si la clave de dependencias existe
+if (!isset($appConfig['dependencies'])) {
+    die('Error: La configuración no contiene la clave "dependencies".');
+}
+
+// Inicia la aplicación
+try {
+    return Application::init($appConfig)->getServiceManager();
+} catch (\Throwable $e) {
+    die('Error en la inicialización: ' . $e->getMessage());
+}
